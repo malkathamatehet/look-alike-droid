850a3eae72eb4d4cf5e0cf1eca8c5b2daa4137a7
Mika Gazit <mikagith@gmail.com>
2024-11-04 01:26:44 -0500

    Add movement and delay to motor control

--- motor_control.py
+++ motor_control.py
@@ -3,6 +3,7 @@ import busio
 import adafruit_pca9685
 i2c = busio.I2C(board.SCL, board.SDA)
 hat = adafruit_pca9685.PCA9685(i2c)
+import time
 
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
@@ -12,4 +13,11 @@ elbow = kit.servo[1]
 
 shoulder.actuation_range = 45
 elbow.actuation_range = 90
+
+shoulder.angle = 0
+elbow.angle = 0
+time.sleep(.5)
+shoulder.angle = 45
+elbow.angle = 90
+
 
